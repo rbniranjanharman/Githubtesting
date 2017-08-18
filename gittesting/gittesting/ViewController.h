9038ff51c33849d3f3b493e850d29cf15f11f8c2
Niranjan <rniranjan@itadmins-MacBook-Pro-2.local>
2017-08-18 19:55:55 +0530

    Initial commit

---
+++ gittesting/gittesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittesting
+//
+//  Created by Niranjan, Rajabhaiya on 18/08/17.
+//  Copyright © 2017 harman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gittesting/gittesting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittesting
+//
+//  Created by Niranjan, Rajabhaiya on 18/08/17.
+//  Copyright © 2017 harman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
